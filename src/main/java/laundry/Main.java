f19930d8cf5f0122cb1758320aacf3f123842b79
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-10-23 10:52:14 +0200

    Move to package "laundry"

--- src/main/java/laundry/Main.java
+++ src/main/java/laundry/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package laundry;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/laundry/WashingMachine.java
+++ src/main/java/laundry/WashingMachine.java
@@ -1,4 +1,4 @@
-package org.example;
+package laundry;
 
 public class WashingMachine {
 
